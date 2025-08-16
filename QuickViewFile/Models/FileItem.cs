4d6e92a712b9e4c52dafe389497b2715cdfeacb5
Michal Latynski <michal@latynski.pl>
2025-08-16 20:09:03 +0200

    FileItem model; VIewModel for main view; MainWindow binding

---
+++ QuickViewFile/Models/FileItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickViewFile.Models
+{
+    public class FileItem
+    {
+        public string Name { get; set; } // Name of the file with extension
+        public long Size { get; set; } // File size in bytes
+        public string FullPath { get; set; } // Full path of the file to preview
+
+    }
+}
---
+++ QuickViewFile/ViewModel/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using QuickViewFile.Models;
+using System.Collections.ObjectModel;
+using System.IO;
+
+public class MainViewModel
+{
+    public ObservableCollection<FileItem> Files { get; set; } = new();
+
+    private FileItem _selectedFile;
+    public FileItem SelectedFile
+    {
+        get => _selectedFile;
+        set
+        {
+            _selectedFile = value;
+            
+        }
+    }
+
+    public MainViewModel()
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        var filesInCurrentDirectory = new DirectoryInfo(currentDir).GetFiles();
+
+        foreach (var file in filesInCurrentDirectory)
+        {
+            Files.Add(new FileItem
+            {
+                Name = file.Name,
+                Size = file.Length,
+                FullPath = file.FullName
+            });
+        }
+    }
+}
