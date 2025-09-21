139d845cc68b82a4b9e6c36ec869c657dca6fefd
Michal Latynski <michal@latynski.pl>
2025-09-22 01:40:09 +0200

    New control - ZoomableMediaElement (based on ZoomableImage but redesigned to video)

--- QuickViewFile/Controls/VideoPlayerFullScreen.xaml.cs
+++ QuickViewFile/Controls/VideoPlayerFullScreen.xaml.cs
@@ -42,7 +42,7 @@ namespace QuickViewFile.Controls
         public VideoPlayerFullScreen(string filePath)
         {
             InitializeComponent();
-
+            //videoFullScreenPlayer.Height = heigth;
             //_config = ConfigHelper.LoadConfig();
             DispatcherTimer timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
@@ -54,22 +54,22 @@ namespace QuickViewFile.Controls
 
         public void StartPlaying(string filePath)
         {
-            videoPlayer.Source = new Uri(filePath);
-            videoPlayer.Play();
+            videoFullScreenPlayer.Source = new Uri(filePath);
+            videoFullScreenPlayer.Play();
             isVideoPaused = false;
             mediaPlayerIsPlaying = true;
-            videoPlayer.Volume = 1;
-            //videoPlayer.Height = _config.VideoHeigth;
-            //videoPlayer.RenderSize = new Size(_config.VideoWidth, _config.VideoHeigth);
+            videoFullScreenPlayer.Volume = 1;
+            //videoFullScreenPlayer.Height = _config.VideoHeigth;
+            //videoFullScreenPlayer.RenderSize = new Size(_config.VideoWidth, _config.VideoHeigth);
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            if ((videoPlayer.Source != null) && (videoPlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            if ((videoFullScreenPlayer.Source != null) && (videoFullScreenPlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
             {
                 sliProgress.Minimum = 0;
-                sliProgress.Maximum = videoPlayer.NaturalDuration.TimeSpan.TotalSeconds;
-                sliProgress.Value = videoPlayer.Position.TotalSeconds;
+                sliProgress.Maximum = videoFullScreenPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliProgress.Value = videoFullScreenPlayer.Position.TotalSeconds;
             }
         }
 
@@ -83,9 +83,9 @@ namespace QuickViewFile.Controls
         {
             if (mediaPlayerIsPlaying)
             {
-                videoPlayer.Stop();
+                videoFullScreenPlayer.Stop();
             }
-            videoPlayer.Play();
+            videoFullScreenPlayer.Play();
             isVideoPaused = false;
             mediaPlayerIsPlaying = true;
         }
@@ -106,7 +106,7 @@ namespace QuickViewFile.Controls
 
         private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            videoPlayer.Pause();
+            videoFullScreenPlayer.Pause();
             isVideoPaused = true;
             mediaPlayerIsPlaying = false;
         }
@@ -119,7 +119,7 @@ namespace QuickViewFile.Controls
 
         private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            videoPlayer.Stop();
+            videoFullScreenPlayer.Stop();
             isVideoPaused = true;
             mediaPlayerIsPlaying = false;
         }
@@ -132,32 +132,32 @@ namespace QuickViewFile.Controls
         private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             userIsDraggingSlider = false;
-            videoPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+            videoFullScreenPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
         }
 
         private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            videoPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+            videoFullScreenPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
             lblProgressStatus.Text = TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
             fullTime.Text = TimeSpan.FromSeconds(sliProgress.Maximum).ToString(@"hh\:mm\:ss");
         }
 
         private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            videoPlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+            videoFullScreenPlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
         }
 
         private void PlayOrPauseMedia()
         {
             if (!isVideoPaused)
             {
-                videoPlayer.Pause();
+                videoFullScreenPlayer.Pause();
                 isVideoPaused = true;
                 mediaPlayerIsPlaying = false;
             }
             else
             {
-                videoPlayer.Play();
+                videoFullScreenPlayer.Play();
                 isVideoPaused = false;
                 mediaPlayerIsPlaying = true;
             }
@@ -191,23 +191,23 @@ namespace QuickViewFile.Controls
             }
             else if (key == Key.Add)
             {
-                videoPlayer.Volume += 0.1;
+                videoFullScreenPlayer.Volume += 0.1;
             }
             else if (key == Key.Subtract)
             {
-                videoPlayer.Volume -= 0.1;
+                videoFullScreenPlayer.Volume -= 0.1;
             }
             else if (key == Key.Left)
             {
-                TimeSpan actualTime = videoPlayer.Position;
+                TimeSpan actualTime = videoFullScreenPlayer.Position;
                 TimeSpan newTime = actualTime.Subtract(TimeSpan.FromSeconds(10));
-                videoPlayer.Position = newTime;
+                videoFullScreenPlayer.Position = newTime;
             }
             else if (key == Key.Right)
             {
-                TimeSpan actualTime = videoPlayer.Position;
+                TimeSpan actualTime = videoFullScreenPlayer.Position;
                 TimeSpan newTime = actualTime.Add(TimeSpan.FromSeconds(10));
-                videoPlayer.Position = newTime;
+                videoFullScreenPlayer.Position = newTime;
             }
         }
 
@@ -217,9 +217,9 @@ namespace QuickViewFile.Controls
             {
                 if (disposing)
                 {
-                    videoPlayer.Stop();
+                    videoFullScreenPlayer.Stop();
                     mediaPlayerIsPlaying = false;
-                    videoPlayer.Close();
+                    videoFullScreenPlayer.Close();
                 }
 
                 // TODO: free unmanaged resources (unmanaged objects) and override finalizer
@@ -229,7 +229,7 @@ namespace QuickViewFile.Controls
         }
 
         // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~VideoPlayerControl()
+        // ~videoFullScreenPlayerControl()
         // {
         //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
         //     Dispose(disposing: false);
---
+++ QuickViewFile/Controls/ZoomableMediaElement.cs
@@ -0,0 +1,117 @@
+﻿using QuickViewFile.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace QuickViewFile.Controls
+{
+    public class ZoomableMediaElement : MediaElement
+    {
+        private readonly ConfigModel _config;
+        private Point? lastDragPoint;
+        private double currentScale = 1.0;
+        private TranslateTransform translateTransform = new TranslateTransform();
+        private ScaleTransform scaleTransform = new ScaleTransform();
+        private TransformGroup transformGroup = new TransformGroup();
+
+        public ZoomableMediaElement()
+        {
+            this.UseLayoutRounding = true;
+            transformGroup.Children.Add(scaleTransform);
+            transformGroup.Children.Add(translateTransform);
+            this.ClipToBounds = true;
+
+            this.RenderTransform = transformGroup;
+            this.RenderTransformOrigin = new Point(0, 0);
+
+            this.MouseLeftButtonDown += Image_MouseLeftButtonDown;
+            this.MouseLeftButtonUp += Image_MouseLeftButtonUp;
+            this.MouseMove += Image_MouseMove;
+            this.MouseWheel += Image_MouseWheel;
+            this.SizeChanged += ZoomableImage_SizeChanged;
+        }
+
+        private void ZoomableImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            ClampTranslation();
+        }
+
+        private void ClampTranslation()
+        {
+            double imageWidth = this.NaturalVideoWidth * currentScale;
+            double imageHeight = this.NaturalVideoHeight * currentScale;
+            double viewportWidth = this.ActualWidth;
+            double viewportHeight = this.ActualHeight;
+
+            double maxX = Math.Max((imageWidth - viewportWidth), 0);
+            double maxY = Math.Max((imageHeight - viewportHeight), 0);
+
+            // Clamp so the image cannot be dragged outside the visible area
+            translateTransform.X = Math.Min(Math.Max(translateTransform.X, -maxX), maxX);
+            translateTransform.Y = Math.Min(Math.Max(translateTransform.Y, -maxY), maxY);
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lastDragPoint = e.GetPosition(this);
+            this.CaptureMouse();
+        }
+
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            lastDragPoint = null;
+            this.ReleaseMouseCapture();
+        }
+
+        private void Image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (lastDragPoint.HasValue && this.IsMouseCaptured)
+            {
+                Point currentPoint = e.GetPosition(this);
+                Vector delta = Point.Subtract(currentPoint, lastDragPoint.Value);
+
+                translateTransform.X += delta.X;
+                translateTransform.Y += delta.Y;
+
+                ClampTranslation();
+                lastDragPoint = currentPoint;
+            }
+        }
+
+        private void Image_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(this);
+            double zoomFactor = e.Delta > 0 ? 1.1 : 1 / 1.1;
+
+            double newScale = currentScale * zoomFactor;
+            if (newScale < 1)
+                newScale = 1;
+            if (newScale > 100)
+                newScale = 100;
+
+            if (Math.Abs(newScale - currentScale) < 0.001)
+                return;
+
+            // Calculate point from where we will scale
+            Point relativePoint = new Point(
+                mousePosition.X / this.ActualWidth,
+                mousePosition.Y / this.ActualHeight);
+
+            // Save current position before scalling
+            double absoluteX = mousePosition.X * currentScale + translateTransform.X;
+            double absoluteY = mousePosition.Y * currentScale + translateTransform.Y;
+
+            // Scale
+            scaleTransform.ScaleX = newScale;
+            scaleTransform.ScaleY = newScale;
+
+            // Calculate new translation to keep the point under the mouse
+            translateTransform.X = absoluteX - mousePosition.X * newScale;
+            translateTransform.Y = absoluteY - mousePosition.Y * newScale;
+
+            currentScale = newScale;
+            ClampTranslation();
+        }
+    }
+}
