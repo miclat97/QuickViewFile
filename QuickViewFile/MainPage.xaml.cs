e0e906302ad51214e561e829f7ba16c05bd5f4ea
Michal Latynski <michal@latynski.pl>
2025-09-22 03:00:06 +0200

    Page within Window

---
+++ QuickViewFile/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using QuickViewFile.Controls;
+using QuickViewFile.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickViewFile
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        private bool _filesListViewVisible = true;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var vm = new FilesListViewModel(Directory.GetCurrentDirectory());
+
+            DataContext = vm;
+
+        }
+        private void FilesListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) //Change directory or force load file (using double click)
+        {
+            if (DataContext is FilesListViewModel vm)
+            {
+                if (sender is ListView listView && listView.SelectedItem is QuickViewFile.Models.ItemList file)
+                {
+
+                    Application.Current.Dispatcher.BeginInvoke(() =>
+                    {
+                        vm.OnFileDoubleClick(file);
+                    });
+                }
+            }
+        }
+
+        private void AppWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (DataContext is FilesListViewModel vm)
+            {
+                if (sender is ListView listView && listView.SelectedItem is QuickViewFile.Models.ItemList file)
+                {
+                    if (e.Key == Key.Enter)
+                    {
+                        Application.Current.Dispatcher.BeginInvoke(() =>
+                        {
+                            vm.OnFileDoubleClick(file);
+                        });
+                    }
+                }
+                if (TextBoxTextContent.Text is not null)
+                {
+                    if (e.Key == Key.Add)
+                    {
+                        TextBoxTextContent.FontSize += 0.5;
+                    }
+                    else if (e.Key == Key.Subtract)
+                    {
+                        TextBoxTextContent.FontSize -= 0.5;
+                    }
+                }
+            }
+        }
+
+        private void Grid_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (DataContext is FilesListViewModel vm)
+                {
+                    if (vm.SelectedItem?.FileContentModel.VideoMedia is not null)
+                    {
+                        VideoPlayerFullScreen fullScreenVideo = new VideoPlayerFullScreen(vm.SelectedItem.FullPath);
+                        vm.SelectedItem = null;
+                        fullScreenVideo.Show();
+                    }
+                    else
+                    {
+                        if (_filesListViewVisible)
+                        {
+                            FilesListView.Visibility = Visibility.Collapsed;
+                            MainWindowGridSplitter.Visibility = Visibility.Collapsed;
+                            FileFullPathTextBlock.Visibility = Visibility.Collapsed;
+                            _filesListViewVisible = false;
+                        }
+                        else
+                        {
+                            FilesListView.Visibility = Visibility.Visible;
+                            MainWindowGridSplitter.Visibility = Visibility.Visible;
+                            FileFullPathTextBlock.Visibility = Visibility.Visible;
+                            _filesListViewVisible = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
--- QuickViewFile/MainWindow.xaml.cs
+++ QuickViewFile/MainWindow.xaml.cs
@@ -8,95 +8,11 @@ using System.Windows.Navigation;
 
 namespace QuickViewFile
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
-        private bool _filesListViewVisible = true;
         public MainWindow()
         {
             InitializeComponent();
-            var vm = new FilesListViewModel(Directory.GetCurrentDirectory());
-
-            DataContext = vm;
-        }
-
-        private void FilesListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e) //Change directory or force load file (using double click)
-        {
-            if (DataContext is FilesListViewModel vm)
-            {
-                if (sender is ListView listView && listView.SelectedItem is QuickViewFile.Models.ItemList file)
-                {
-
-                    Application.Current.Dispatcher.BeginInvoke(() =>
-                    {
-                        vm.OnFileDoubleClick(file);
-                    });
-                }
-            }
-        }
-
-        private void AppWindow_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (DataContext is FilesListViewModel vm)
-            {
-                if (sender is ListView listView && listView.SelectedItem is QuickViewFile.Models.ItemList file)
-                {
-                    if (e.Key == Key.Enter)
-                    {
-                        Application.Current.Dispatcher.BeginInvoke(() =>
-                        {
-                            vm.OnFileDoubleClick(file);
-                        });
-                    }
-                }
-                if (TextBoxTextContent.Text is not null)
-                {
-                    if (e.Key == Key.Add)
-                    {
-                        TextBoxTextContent.FontSize += 0.5;
-                    }
-                    else if (e.Key == Key.Subtract)
-                    {
-                        TextBoxTextContent.FontSize -= 0.5;
-                    }
-                }
-            }
-        }
-
-        private void Grid_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            try
-            {
-                if (DataContext is FilesListViewModel vm)
-                {
-                    if (vm.SelectedItem?.FileContentModel.VideoMedia is not null)
-                    {
-                        VideoPlayerFullScreen fullScreenVideo = new VideoPlayerFullScreen(vm.SelectedItem.FullPath);
-                        vm.SelectedItem = null;
-                        fullScreenVideo.Show();
-                    }
-                    else
-                    {
-                        if (_filesListViewVisible)
-                        {
-                            FilesListView.Visibility = Visibility.Collapsed;
-                            MainWindowGridSplitter.Visibility = Visibility.Collapsed;
-                            FileFullPathTextBlock.Visibility = Visibility.Collapsed;
-                            _filesListViewVisible = false;
-                        }
-                        else
-                        {
-                            FilesListView.Visibility = Visibility.Visible;
-                            MainWindowGridSplitter.Visibility = Visibility.Visible;
-                            FileFullPathTextBlock.Visibility = Visibility.Visible;
-                            _filesListViewVisible = true;
-                        }
-                    }
-                }
-            }
-            catch
-            {
-
-            }
         }
     }
 }
